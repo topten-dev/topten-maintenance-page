81585b2dc846b5fd09457cef9cc8da04a44df74b
Victor Gonzalez <victor@vgr.cl>
2025-07-04 16:54:51 +0200

    add github pages
    Signed-off-by: Victor Gonzalez <victor@vgr.cl>

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: '/topten-maintenance-page/', // Set base for GitHub Pages
     plugins: [
         tailwindcss(),
     ],
