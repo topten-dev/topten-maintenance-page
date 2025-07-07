2fee2e1dad2ccd36d0de44107372001899efcca9
Victor Gonzalez <victor@vgr.cl>
2025-07-07 11:54:18 +0200

    add compatibility for Cloudflare pages
    Signed-off-by: Victor Gonzalez <victor@vgr.cl>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        cloudflare(),
     ],
 })
